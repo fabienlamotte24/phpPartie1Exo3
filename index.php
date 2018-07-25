4e37c20e769bef73761b58ef80b8fe92e8a18da0
fabienlamotte24 <fabien.lamotte00@outlook.fr>
2018-07-25 14:13:11 +0200

    php partie 1 exo 3

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice 3</title>
+  </head>
+  <body>
+    <?php
+      $km = 1;
+      echo $km . '<br />';
+      $km = 3;
+      echo $km . '<br />';
+      $km = 125;
+      echo $km;
+     ?>
+  </body>
+</html>
